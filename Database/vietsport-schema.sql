7185a7e30353ea28b5eda648ae5b81aab7b57ec8
NguyenTraMyHCMUS <23120300@student.hcmus.edu.vn>
2026-01-16 14:59:36 +0700

    thay đổi thuộc tính NhanVien trong HoaDon

--- Database/vietsport-schema.sql
+++ Database/vietsport-schema.sql
@@ -5,8 +5,8 @@ DATABASE SCHEMA - SPORTS CENTER
 --ALTER DATABASE VietSport SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 --DROP DATABASE VietSport;
 
---CREATE DATABASE VietSport
-
+CREATE DATABASE VietSport
+go
 USE VietSport
 -- 1. BẢNG: QUẢN LÝ NGƯỜI DÙNG --
 
@@ -655,3 +655,5 @@ alter table GoiDichVu
 add check (
     LoaiTaiSan in (N'Phòng tắm VIP', N'Tủ đồ')
 );
+ALTER TABLE HoaDon
+ALTER COLUMN MaNhanVien char(10) NULL;
